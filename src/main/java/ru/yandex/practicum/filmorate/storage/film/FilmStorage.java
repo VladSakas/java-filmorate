34b0ded3ba55c26c0ee3777544d8eaa662e5b028
kirill <kirillkomlyashov@yandex.ru>
2026-06-23 16:48:59 +0300

    feat: add film get common

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage.film;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
@@ -21,4 +22,6 @@ public interface FilmStorage {
     void removeLike(Long filmId, Long userId);
 
     Collection<Film> getTopFilms(int count);
+
+    List<Film> getCommonFilms(Integer userId, Integer friendId);
 }
