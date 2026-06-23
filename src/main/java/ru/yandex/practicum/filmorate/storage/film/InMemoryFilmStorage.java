37c49704d1d04e6797fb8b4b9a49a182074dd14c
kirill <kirillkomlyashov@yandex.ru>
2026-06-23 16:58:53 +0300

    fix: use Long instead of Integer for user IDs in getCommonFilms

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -80,8 +80,11 @@ public class InMemoryFilmStorage implements FilmStorage {
 
     @Override
     public List<Film> getCommonFilms(Integer userId, Integer friendId) {
+        Long userIdLong = userId.longValue();
+        Long friendIdLong = friendId.longValue();
+
         return films.values().stream()
-                .filter(film -> film.getLikes().contains(userId) && film.getLikes().contains(friendId))
+                .filter(film -> film.getLikes().contains(userIdLong) && film.getLikes().contains(friendIdLong))
                 .collect(Collectors.toList());
     }
 
