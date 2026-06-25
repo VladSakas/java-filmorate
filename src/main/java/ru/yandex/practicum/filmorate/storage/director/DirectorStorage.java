8f528a071dc269cf52fceb2e63dd827e3d4b56fa
kirill <kirillkomlyashov@yandex.ru>
2026-06-25 17:09:27 +0300

    fix: remove unused import

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.director;
 import ru.yandex.practicum.filmorate.model.Director;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 public interface DirectorStorage {
